b7070f0bd51179cfcf9f81f414b3f90659dc7088
Mayukh12397 <bagariamayukh800@gmail.com>
2026-02-26 13:12:00 +0530

    Lesson1

---
+++ Lesson1/basicchatbot.py
@@ -0,0 +1,13 @@
+print("Hello! I am an AI bot")
+name= input("What's your name?")
+print("Nice to meet you", name)
+feeling= input("How are you feeling today?").lower()
+if feeling=="good" or feeling=="great":
+    print("I am glad to hear that!")
+elif "bad" in feeling.lower():
+    print("I hope your day gets better!")
+else:
+    print("I see. Sometimes its hard to put feelings into words.")
+# hobby
+
+    
