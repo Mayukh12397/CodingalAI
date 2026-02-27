69e39d242371fc54f8a900ffb37a81d7190e3fac
Mayukh12397 <bagariamayukh800@gmail.com>
2026-02-27 12:01:16 +0530

    Homework

---
+++ Lesson1/AIChatbot.py
@@ -0,0 +1,20 @@
+print("Hello! I am an AI CHATBOT!")
+name= input("What is your name?")
+print("Nice to meet you", name)
+feeling= input("How are you feeling today?")
+if feeling== "good" or feeling== "great":
+    print("I am really glad to hear that")
+elif feeling== "bad":
+    print("I hope you have a better day!")
+else:
+    print("Sometimes you cannot put your emotions into words.")
+hobby= input("What's your hobby?")
+print("That is a good hobby to have.")
+my_list= ['automation','decision-making','computervision','generative']
+
+print("MY FEATURES ARE AS FOLLOWS:-")
+print(my_list[0])
+print(my_list[1])
+print(my_list[2])
+print(my_list[3])
+print("It was nice chatting with you!", name)
