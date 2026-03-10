79079260af70c86f39a4ada871d361f13076be1c
Mayukh12397 <bagariamayukh800@gmail.com>
2026-03-10 18:57:30 +0530

    basicImageManipulation

---
+++ Module2/Lesson8/homework.py
@@ -0,0 +1,54 @@
+# Basics of Image Manipulation
+# Cropping, Rotating and Brightness Adjustment
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+image = cv2.imread('example.jpg')
+
+# Convert BGR to RGB for displaying
+imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+plt.imshow(imageRGB)
+plt.title("Original Image")
+plt.show()
+
+
+croppedImage = image[100:400, 250:450]
+
+croppedImageRGB = cv2.cvtColor(croppedImage, cv2.COLOR_BGR2RGB)
+
+plt.imshow(croppedImageRGB)
+plt.title("Cropped Image")
+plt.show()
+
+
+
+#  Rotate the Image
+(h, w) = image.shape[:2]
+
+center = (w//2, h//2)
+
+rotationMatrix = cv2.getRotationMatrix2D(center, 90, 1.0)
+
+rotatedImage = cv2.warpAffine(image, rotationMatrix, (w, h))
+
+rotatedRGB = cv2.cvtColor(rotatedImage, cv2.COLOR_BGR2RGB)
+
+plt.imshow(rotatedRGB)
+plt.title("Rotated Image (90 degrees)")
+plt.show()
+
+
+# Increase Brightness
+brightnessMatrix = np.ones(image.shape, dtype="uint8") * 90
+
+brighterImage = cv2.add(image, brightnessMatrix)
+
+brighterRGB = cv2.cvtColor(brighterImage, cv2.COLOR_BGR2RGB)
+
+plt.imshow(brighterRGB)
+plt.title("Brighter Image")
+plt.show()
