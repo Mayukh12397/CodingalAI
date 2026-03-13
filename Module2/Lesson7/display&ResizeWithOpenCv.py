0b56090e530cfb7efe983870b7484841457c5990
Mayukh12397 <bagariamayukh800@gmail.com>
2026-03-13 12:40:00 +0530

    FunWithFilters

--- Module2/Lesson7/display&ResizeWithOpenCv.py
+++ Module2/Lesson7/display&ResizeWithOpenCv.py
@@ -6,5 +6,5 @@ cv2.namedWindow('loaded image', cv2.WINDOW_NORMAL) # it creates a window where y
 cv2.resizeWindow('loaded image', 800,500) # sets the size to 800*500 pixels and its also resizable.(width- 800, height - 500)
 cv2.imshow('loaded image', image ) # it displays the image in the created window with the set credentials of the size of the image. Arguements are the data you are passing in the function. e.g- in imshow function, image variable, created window(loaded image).
 cv2.waitKey(0) # 0 represents indefinity and it will wait indefinetely until a key is pressed.
-cv2.destroyAllWindows() #close the windows. If there is any open window, it should be closed properly.
+cv2.destroyAllWindows() # close the windows. If there is any open window, it should be closed properly.
 print("imagedimension: ", image.shape) # it will just print the dimensions of the image in the terminal.
